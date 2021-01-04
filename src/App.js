b7528b11e2a66bc82e9e7e9d68f33a1c7663de62
anshuldaksh2016 <anshuldaksh2016@gmail.com>
2021-01-04 16:02:46 +0530

    rendering everything and optimizing the code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const prices = {
+  Apple: 20,
+  Banana: 5,
+  Mango: 15,
+  Papaya: 50,
+}
+
+const fruits = ["Apple", "Mango", "Banana", "Papaya"];
+
+const initialState = {};
+fruits.forEach(fruit => {
+
+  const price = prices[fruit];
+  initialState[fruit] = {
+    count: 0,
+    price,
+  }
+})
+
+console.log(initialState)
 function App() {
+
+  //const [applesCount, setApplesCount] = useState(1);
+  //const [mangoCount, setMangoCount] = useState(0);
+  //const [BananaCount, setBananaCount] = useState(0);
+
+
+  const [basket, setBasket] = useState(initialState);
+
+  let totalFruitsCount = 0;
+  let totalFruitsPrice = 0;
+
+
+  Object.keys(basket).forEach(fruitKey => {
+    const fruit = basket[fruitKey];
+    totalFruitsCount += fruit.count;
+    totalFruitsPrice += fruit.count * fruit.price;
+
+  })
+
+
+
+
+  const taxPercent = 20;
+  const taxPrice = (totalFruitsPrice * taxPercent) / 100;
+
+  function addFruitToBasket(fruitKey) {
+    console.log(fruitKey);
+    // use the setbasket update fxn
+    setBasket({
+      ...basket,
+      [fruitKey]: {
+        ...basket[fruitKey],
+        count: basket[fruitKey].count + 1,
+      },
+    });
+  }
+  function removeFruitsFromBasket(fruitKey) {
+    console.log(fruitKey);
+    setBasket({
+      ...basket,
+      [fruitKey]: {
+        ...basket[fruitKey],
+        count: basket[fruitKey].count - 1,
+      }
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="fruits-shop">
+        <h2>Fruit Shop</h2>
+
+        {
+          Object.keys(basket).map(fruitKey => {
+            const fruit = basket[fruitKey];
+            return (
+              <div>
+                <button onClick={() => addFruitToBasket(fruitKey)}>+</button>
+                <button onClick={() => removeFruitsFromBasket(fruitKey)}>-</button>
+                {fruitKey} :{fruit.count}
+              </div>
+            )
+          })
+        }
+
+      </div>
+
+
+      <hr />
+
+
+      {/* invoice  */}
+
+      <h3>Invoice</h3>
+      <div>Total fruits in the basket : {totalFruitsCount}</div>
+      <div>Total fruits price : {totalFruitsPrice} </div>
+      <div>Tax percentage : 20%</div>
+      <div>Tax Price : {taxPrice}</div>
+      <br />
+      <div>TOtal price :{taxPrice + totalFruitsPrice} </div>
     </div>
   );
 }
